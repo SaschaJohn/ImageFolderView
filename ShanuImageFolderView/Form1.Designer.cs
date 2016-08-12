b8eefbfb4a6594862cbb896678e79a59f577b3cd
Sascha <Sascha@I5>
2016-08-12 17:50:42 +0200

    Add project files.

---
+++ ShanuImageFolderView/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ShanuImageFolderView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.pnControls = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton4,
+            this.toolStripButton1,
+            this.toolStripButton3,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1264, 71);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // pnControls
+            // 
+            this.pnControls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnControls.AutoScroll = true;
+            this.pnControls.BackColor = System.Drawing.Color.White;
+            this.pnControls.Location = new System.Drawing.Point(0, 95);
+            this.pnControls.Name = "pnControls";
+            this.pnControls.Size = new System.Drawing.Size(741, 634);
+            this.pnControls.TabIndex = 18;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateGray;
+            this.button1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(936, 673);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 45);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Save privew Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(856, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 23);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Move on Image for Image Privew ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(747, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 557);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::ShanuImageFolderView.Properties.Resources.photos_20;
+            this.toolStripButton4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(24, 68);
+            this.toolStripButton4.Text = "toolStripButton9";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(36, 68);
+            this.toolStripButton1.Text = "toolStripButton9";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(52, 68);
+            this.toolStripButton3.Text = "toolStripButton9";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(68, 68);
+            this.toolStripButton5.Text = "toolStripButton9";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 729);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnControls);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image Folder View - Syed Shanu / syedshanumcain@gmail.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.Panel pnControls;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+    }
+}
+
---
+++ ShanuImageFolderView/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ShanuImageFolderView
+{
+
+    public partial class Form1 : Form
+    {
+            private System.Windows.Forms.FolderBrowserDialog folderBrowserDlg;
+            int locX = 20;
+            int locY = 10;
+            int sizeWidth = 80;
+            int sizeHeight = 80;
+        List<PictureBox> previews = new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo Folder;
+            FileInfo[] Images;
+            this.folderBrowserDlg.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDlg.ShowNewFolderButton = false;
+            DialogResult result = this.folderBrowserDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Folder = new DirectoryInfo(folderBrowserDlg.SelectedPath);
+                Images = Folder.GetFiles();
+
+                pnControls.Controls.Clear();
+
+                int locnewX = locX;
+                int locnewY = locY;
+                foreach (FileInfo img in Images)
+                {
+
+                    if (img.Extension.ToLower() == ".png" || img.Extension.ToLower() == ".jpg" || img.Extension.ToLower() == ".gif" || img.Extension.ToLower() == ".jpeg" || img.Extension.ToLower() == ".bmp" || img.Extension.ToLower() == ".tif")
+                    {
+
+                        if (locnewX >= pnControls.Width - sizeWidth - 10)
+                        {
+                            locnewX = locX;
+                            locY = locY + sizeHeight + 30;
+                            locnewY = locY;
+                        }
+                        else
+                        {
+
+                            locnewY = locY;
+                        }
+
+                        loadImagestoPanel(img.Name, img.FullName, locnewX,locnewY);
+                        locnewY = locY + sizeHeight + 10;
+                        locnewX = locnewX + sizeWidth + 10;
+
+                        
+                    }
+                }
+            }
+           
+        }
+
+        private void loadImagestoPanel(String imageName,String ImageFullName,int newLocX,int newLocY)
+        {      
+                PictureBox ctrl = new PictureBox();
+                ctrl.Image = Image.FromFile(ImageFullName);
+                ctrl.BackColor = Color.Black;
+              
+                ctrl.Location = new Point(newLocX, newLocY);
+                ctrl.Size = new System.Drawing.Size(sizeWidth, sizeHeight);
+                ctrl.SizeMode = PictureBoxSizeMode.StretchImage;
+                ctrl.MouseMove += new MouseEventHandler(control_MouseMove);
+                ctrl.Click += new EventHandler(control_Click);
+                pnControls.Controls.Add(ctrl);
+            previews.Add(ctrl);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.folderBrowserDlg = new System.Windows.Forms.FolderBrowserDialog();
+            locX = 20;
+            locY = 10;
+            sizeWidth = 80;
+            sizeHeight = 80;
+
+            var Folder = new DirectoryInfo("C:\\Users\\Sascha\\Pictures");
+            var Images = Folder.GetFiles();
+
+            pnControls.Controls.Clear();
+
+            int locnewX = locX;
+            int locnewY = locY;
+            foreach (FileInfo img in Images)
+            {
+
+                if (img.Extension.ToLower() == ".png" || img.Extension.ToLower() == ".jpg" || img.Extension.ToLower() == ".gif" || img.Extension.ToLower() == ".jpeg" || img.Extension.ToLower() == ".bmp" || img.Extension.ToLower() == ".tif")
+                {
+
+                    if (locnewX >= pnControls.Width - sizeWidth - 10)
+                    {
+                        locnewX = locX;
+                        locY = locY + sizeHeight + 30;
+                        locnewY = locY;
+                    }
+                    else
+                    {
+
+                        locnewY = locY;
+                    }
+
+                    loadImagestoPanel(img.Name, img.FullName, locnewX, locnewY);
+                    locnewY = locY + sizeHeight + 10;
+                    locnewX = locnewX + sizeWidth + 10;
+
+
+                }
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {           
+            locX = 20;
+            locY = 10;
+            sizeWidth = 80;
+            sizeHeight = 80;
+            if (pnControls.Controls.Count > 0)
+            {
+                loadControls();                
+            }
+         
+        }
+
+        private void loadControls()
+        {
+            int locnewX = locX;
+            int locnewY = locY;
+
+            foreach (Control p in pnControls.Controls)
+            {
+                if (locnewX >= pnControls.Width - sizeWidth - 10)
+                {
+                    locnewX = locX;
+                    locY = locY + sizeHeight + 30;
+                    locnewY = locY;
+                }
+                else
+                {
+
+                    locnewY = locY;
+                }
+                p.Location = new Point(locnewX, locnewY);
+                p.Size = new System.Drawing.Size(sizeWidth, sizeHeight);
+
+                locnewY = locY + sizeHeight + 10;
+                locnewX = locnewX + sizeWidth + 10;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            int SaveVal = 0;
+            locX = 20;
+            locY = 10;
+            sizeWidth =50;
+            sizeHeight = 50;
+            foreach (Control p in pnControls.Controls)
+            {
+                SaveVal = SaveVal + 1;
+            }
+            if (SaveVal > 0)
+            {
+                loadControls();
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            int SaveVal = 0;
+            locX = 20;
+            locY = 10;
+            sizeWidth = 80;
+            sizeHeight = 80;
+            foreach (Control p in pnControls.Controls)
+            {
+                SaveVal = SaveVal + 1;
+            }
+            if (SaveVal > 0)
+            {
+                loadControls();
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            int SaveVal = 0;
+            locX = 20;
+            locY = 10;
+            sizeWidth = 120;
+            sizeHeight = 120;
+            foreach (Control p in pnControls.Controls)
+            {
+                SaveVal = SaveVal + 1;
+            }
+            if (SaveVal > 0)
+            {
+                loadControls();
+            }
+        }
+
+
+        private void control_Click(object sender, EventArgs e)
+        {
+            foreach(var q in previews){
+
+                q.BackColor = System.Drawing.SystemColors.ActiveCaption;
+                q.Padding = new System.Windows.Forms.Padding(0);
+                q.Refresh();
+            }
+
+           
+
+            PictureBox p = (PictureBox)sender;
+
+            p.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            p.Padding = new System.Windows.Forms.Padding(3);
+            p.Refresh();
+            return;
+        }
+
+        private void control_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            
+
+        Control control = (Control)sender;
+                PictureBox pic = (PictureBox)control;
+                pictureBox1.Image = pic.Image;
+
+            if (pictureBox1.Tag == null)
+            {
+                pictureBox1.Tag = Color.Blue;
+                pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox1.Padding = new Padding(5);
+                pictureBox1.Refresh();
+                return;
+            }
+
+            if ((Color)pictureBox1.Tag == Color.Red) { pictureBox1.Tag = Color.Blue; }
+            else { pictureBox1.Tag = Color.Red; }
+            pictureBox1.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pnControls.Width, pnControls.Height);
+            pnControls.DrawToBitmap(bmp, new Rectangle(0, 0, pnControls.Width, pnControls.Height));
+            SaveFileDialog dlg = new SaveFileDialog();
+            // dlg.Filter = "JPG Files (*.JPG)|*.JPG";
+            dlg.FileName = "*";
+            dlg.DefaultExt = "bmp";
+            dlg.ValidateNames = true;
+            dlg.Filter = "Bitmap Image (.bmp)|*.bmp|Gif Image (.gif)|*.gif |JPEG Image (.jpeg)|*.jpeg |Png Image (.png)|*.png";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(dlg.FileName);
+            }           
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            int SaveVal = 0;
+            locX = 20;
+            locY = 10;
+            sizeWidth = 160;
+            sizeHeight = 160;
+            foreach (Control p in pnControls.Controls)
+            {
+                SaveVal = SaveVal + 1;
+            }
+            if (SaveVal > 0)
+            {
+                loadControls();
+            }
+        }
+    }
+}
